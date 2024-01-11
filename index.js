3869df1d0b57acd3f7def7b4085ad5e793f21480
BoltR6 <johnwallacewhite@gmail.com>
2024-01-10 21:38:08 -0600

    Added home splash

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 
 app.use('/public', express.static(__dirname+'/public'));
 app.get('/:social', (req, res) => {
-	// Return index.html file
 	res.sendFile(__dirname + `/public/pages/${req.params.social}.html`);
+});
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + `/public/index.html`);
 });
 app.listen(4000, () => {
   console.log('Server running on port 4000');
